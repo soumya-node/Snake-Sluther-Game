16bab188adf1b686df802928c2429c8644174129
soumya-ranjan-samal <soumyaranjan1405@gmail.com>
2025-12-18 18:33:35 +0530

    added the db connection

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {configDotenv} from "dotenv";
+import { connectDB } from "./databaseconnection.js";
+
+configDotenv();
+
+connectDB().then(()=>{
+    console.log("database connected...");
+}).catch((error)=>{
+    console.log("Database connection error ", error);
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+app.get('/', (req,res)=>{
+    res.status(200).json({
+        message: "hello ji!"
+    });
+});
+
+app.listen(port,()=>{
+    console.log('Server Started Listening...');
+});
